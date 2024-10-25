4ce81bb7d178878bb2a0f88a2c0a2486c3f4df35
Alvan <alvanmarvianto@gmail.com>
2024-10-25 20:29:56 +0700

    add auth for login

---
+++ js/login.js
@@ -0,0 +1,29 @@
+function authenticateUser() {
+    // Get the values entered by the user
+    const username = document.getElementById("input-username").value;
+    const password = document.getElementById("input-password").value;
+    const loginMessage = document.getElementById("loginMessage");
+
+    // AJAX request to fetch the JSON data
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "js/login.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Parse the JSON data
+            const users = JSON.parse(xhr.responseText).users;
+
+            // Check if user exists and if the password matches
+            const user = users.find(user => user.username === username && user.password === password);
+
+            if (user) {
+                loginMessage.innerHTML = "<p class='text-success'>Login successful!</p>";
+            } else {
+                loginMessage.innerHTML = "<p class='text-danger'>Invalid username or password</p>";
+            }
+        }
+    };
+    
+    xhr.send();
+}
