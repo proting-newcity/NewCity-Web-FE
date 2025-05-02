75f7bc5d53ac6e16cdd1e8e8d5d4878e2bfe0252
Alvan <alvanmarvianto@gmail.com>
2025-05-03 00:55:30 +0700

    add xsrf in header

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
 axios.defaults.baseURL = "https://api.alvanm.my.id/";
+axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
+axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
