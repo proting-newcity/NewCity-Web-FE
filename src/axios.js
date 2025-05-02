83d753645ed1dd009ba2cdf870a115aa926cc8f0
Alvan <alvanmarvianto@gmail.com>
2025-05-03 01:21:05 +0700

    update import axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
+axios.defaults.baseURL         = 'https://api.alvanm.my.id';
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "https://api.alvanm.my.id/";
-axios.defaults.xsrfCookieName = 'XSRF-TOKEN';
-axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
+axios.defaults.xsrfCookieName  = 'XSRF-TOKEN';
+axios.defaults.xsrfHeaderName  = 'X-XSRF-TOKEN';
+
+export default axios;
